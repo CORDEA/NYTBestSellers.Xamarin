b6dd63195bd8d1c5d3c8d3faf3c3d7c7c1b3aec6
Yoshihiro Tanaka <contact@cordea.jp>
2019-04-07 13:18:48 +0900

    prepare unity

--- NYTBestSellers.Android/Application.cs
+++ NYTBestSellers.Android/Application.cs
@@ -5,19 +5,17 @@ using Android.Runtime;
 namespace NYTBestSellers.Android
 {
     [Application]
-    public class App : Application
+    public class Application : global::Android.App.Application
     {
-        public App()
-        {
-        }
-
-        public App(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        public Application(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
         {
         }
 
         public override void OnCreate()
         {
             base.OnCreate();
+
+            App.Initialize();
         }
     }
 }
---
+++ NYTBestSellers/App.cs
@@ -0,0 +1,21 @@
+﻿using Unity;
+
+namespace NYTBestSellers
+{
+    public class App
+    {
+        public static App Current { get; private set; }
+
+        public IUnityContainer Container { get; }
+
+        public static void Initialize()
+        {
+            Current = new App();
+        }
+
+        private App()
+        {
+            Container = new UnityContainer();
+        }
+    }
+}
