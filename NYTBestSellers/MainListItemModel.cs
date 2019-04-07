b57d04bcb68a792f072beff8201b01735caf407e
Yoshihiro Tanaka <contact@cordea.jp>
2019-04-07 20:54:38 +0900

    define model

---
+++ NYTBestSellers/MainListItemModel.cs
@@ -0,0 +1,15 @@
+namespace NYTBestSellers
+{
+    public class MainListItemModel
+    {
+        public MainListItemModel(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public string Title { get; private set; }
+
+        public string Description { get; private set; }
+    }
+}
