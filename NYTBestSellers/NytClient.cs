75434897d21b5a49491b1434c367f8bdbadc70c1
Yoshihiro Tanaka <contact@cordea.jp>
2019-04-07 15:09:38 +0900

    define http client

---
+++ NYTBestSellers/NytClient.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NYTBestSellers
+{
+    public class NytClient
+    {
+        private const string BaseUrl = "https://api.nytimes.com/svc/books/v3/";
+        private const string GetListsPath = "lists.json";
+        private const string GetListNamesPath = "lists/names.json";
+
+        private readonly HttpClient _client = new HttpClient();
+
+        private static string BuildUrl(string path, Dictionary<string, string> query)
+        {
+            return query.Aggregate($"{BaseUrl}{path}?api-key={NytCredentials.ApiToken}",
+                (current, q) => $"{current}&{q.Key}={q.Value}");
+        }
+
+        public async Task<ListNamesResponse> GetListNames()
+        {
+            var result = await _client.GetAsync(BuildUrl(GetListNamesPath, new Dictionary<string, string>()));
+            return JsonConvert.DeserializeObject<ListNamesResponse>(await result.Content.ReadAsStringAsync());
+        }
+    }
+}
