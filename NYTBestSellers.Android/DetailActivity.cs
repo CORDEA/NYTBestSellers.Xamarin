570fa39990b5a66b6df934f0f9c2734a92fee0dd
Yoshihiro Tanaka <contact@cordea.jp>
2019-04-13 16:41:15 +0900

    display details

--- NYTBestSellers.Android/DetailActivity.cs
+++ NYTBestSellers.Android/DetailActivity.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Linq;
+using System.Reactive.Linq;
 using Android.App;
 using Android.Content;
 using Android.OS;
+using Android.Widget;
+using Reactive.Bindings.Extensions;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
 
 namespace NYTBestSellers.Android
 {
@@ -20,6 +26,26 @@ namespace NYTBestSellers.Android
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_detail);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var position = Intent.GetIntExtra(PositionKey, 0);
+
+            var description = FindViewById<TextView>(Resource.Id.description);
+            var author = FindViewById<TextView>(Resource.Id.author);
+            var publisher = FindViewById<TextView>(Resource.Id.publisher);
+            ViewModel.Item
+                .Select(item => item.BookDetail.First())
+                .ObserveOnUIDispatcher()
+                .Subscribe(item =>
+                {
+                    toolbar.Title = item.Title;
+                    description.Text = item.Description;
+                    author.Text = item.Author;
+                    publisher.Text = item.Publisher;
+                });
+
+            ViewModel.OnAttach.Execute(position);
         }
     }
 }
--- NYTBestSellers/DetailViewModel.cs
+++ NYTBestSellers/DetailViewModel.cs
@@ -1,12 +1,23 @@
+using System.Reactive.Linq;
+using Reactive.Bindings;
 using Unity;
 
 namespace NYTBestSellers
 {
     public class DetailViewModel : IViewModel
     {
+        [Dependency] internal ListsRepository Repository { get; set; }
+
+        public ReadOnlyReactiveProperty<ListResponse> Item { get; private set; }
+
+        public readonly ReactiveCommand<int> OnAttach = new ReactiveCommand<int>();
+
         [InjectionMethod]
         internal void Initialize()
         {
+            Item = OnAttach
+                .Select(position => Repository.Find(position))
+                .ToReadOnlyReactiveProperty(mode: ReactivePropertyMode.DistinctUntilChanged);
         }
     }
 }
